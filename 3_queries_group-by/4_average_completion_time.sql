0041ece9ece5b894af59deee460ea84dad651651
Rance Campbell <rancewcampbell@gmail.com>
2020-04-27 21:00:28 +0000

    removed LIMIT on average completion

--- 3_queries_group-by/4_average_completion_time.sql
+++ 3_queries_group-by/4_average_completion_time.sql
@@ -3,5 +3,4 @@ FROM assignment_submissions
 JOIN students
 ON student_id = students.id WHERE students.end_date IS NULL
 GROUP BY students.name
-ORDER BY average_assignment_duration DESC
-LIMIT 10;
+ORDER BY average_assignment_duration DESC;
