4a9d927361f67c2a8b5565adcc245e1a2d2dcae4
Rance Campbell <rancewcampbell@gmail.com>
2020-04-28 21:34:35 +0000

    query for teachers who made an assist plus number of assists

---
+++ 4_queries/12_name_of_teachers_and_number_of_assits.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT teachers.name AS name, cohorts.name AS cohort, COUNT(assistance_requests) AS num_assists
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teachers.name;
